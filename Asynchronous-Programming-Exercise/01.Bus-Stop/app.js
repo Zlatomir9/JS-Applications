1d32203789d13b19d5eec01dae7204c82352d675
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2022-11-02 20:03:30 +0200

    Asynchronous Programming Exercise

---
+++ Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
@@ -0,0 +1,23 @@
+async function getInfo() {
+    const stopIdElement = document.getElementById("stopId");
+    const stopId = stopIdElement.value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+    const stopNameElement = document.getElementById("stopName");
+    const busList = document.getElementById("buses");
+    busList.innerHTML = "";
+    stopId.value = "";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+    
+        stopNameElement.textContent = data.name;
+        Object.entries(data.buses).forEach(([busNumber, arriveTime]) => {
+            const li = document.createElement("li");
+            li.textContent = `Bus ${busNumber} arrives in ${arriveTime} minutes`;
+            busList.appendChild(li);
+        })
+    } catch (error) {
+        stopNameElement.textContent = "Error";
+    }
+}
