4d21884191466cbe6b9d19bac779883e7638533d
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2022-11-05 17:29:22 +0200

    Data-And-Authentication-Exercise

---
+++ Data-and-Authentication-Exercise/01.Messenger/app.js
@@ -0,0 +1,49 @@
+function attachEvents() {
+    document.getElementById("refresh").addEventListener("click", getAllMessages);
+    document.getElementById("submit").addEventListener("click", onSendMsg);
+}
+
+function onSendMsg() {
+    const author = document.querySelector("input[name='author']");
+    const content = document.querySelector("input[name='content']");
+
+    const body = {
+        author: author.value,
+        content: content.value
+    };
+    author.value = "";
+    content.value = "";
+    
+    postMessage(body);
+}
+
+function renderMsg(data) {
+    const textArea = document.getElementById("messages");
+    const content = Object.values(data).map(entry => `${entry.author}: ${entry.content}`).join("\n");
+    textArea.textContent = content;
+}
+
+async function getAllMessages() {
+    const url = "http://localhost:3030/jsonstore/messenger";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    renderMsg(data);
+}
+
+async function postMessage(body) {
+    const url = "http://localhost:3030/jsonstore/messenger";
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+
+    getAllMessages();
+}
+
+attachEvents();
